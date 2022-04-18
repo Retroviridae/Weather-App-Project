10272236833a1734f6b6f12325ffe49275279627
Sam Hendricks <samuelbhendricks@gmail.com>
2022-04-18 17:20:06 -0400

    cleaned up comments, refactored makeCards

--- weatherApp/index.js
+++ weatherApp/index.js
@@ -1,24 +1,13 @@
-// working with clone. TESTING!!!
-// // STRETCH GOAL: Fetching long and lat from location name
 // Austin:  Lat: -97.78262, Long: 30.30086
 // fetch('https://geocode.xyz/austin,tx?json=1')
-// .then(resp => resp.json())
-// .then(json => logCoordinates(json))
 
-// function logCoordinates(json){
-//     console.log(json)
-//     console.log(json.latt)
-//     console.log(json.longt)
-// }
 
 const longForm = document.querySelector('#longitude')
 const latForm = document.querySelector('#latitude')
 const realForm = document.querySelector('#form_location')
 const body = document.querySelector('#mainBody')
-// console.log(body)
-// console.log(longForm)
-// console.log(latForm)
-// console.log(realForm)
+
+
 realForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     getWeather(longForm.value,latForm.value)
@@ -32,37 +21,22 @@ function getWeather (long,lat){
 
 
 function makeCards (weatherObj){
-    // console.log(weatherObj)
-    // console.log(weatherObj.dataseries)
-    const weatherArr = weatherObj.dataseries
-    // console.log(weatherArr)
-    console.log(weatherArr)
-    // console.log(weatherObj.dataseries[0])
-    // console.log(weatherObj.dataseries[0].rh2m)
-    weatherArr.forEach((e)=>{
-        cardMake(e)
-    })
-    function cardMake (e){
+
+    const weatherArray = weatherObj.dataseries
+
+    console.log(weatherArray)
+    weatherArray.forEach((weatherTimePoint) => {
         const card = document.createElement('card')
-        for (key in e){
+        for (key in weatherTimePoint){
             const ul = document.createElement('ul')
-            ul.textContent = `${key}: ${e[key]}`
+            ul.textContent = `${key}: ${weatherTimePoint[key]}`
             card.appendChild(ul)
         }
-        // const dataList = document.createElement('ul')
-        // card.setAttribute('id','timepoint card')
-        // dataList.textContent = `Relative humidity: ${weatherObj.dataseries[0].rh2m}`
-        // card.appendChild(dataList)
         body.appendChild(card)
-    }
+    })
 }
-// getWeather(-97.7,30.3)
 
 
-// getWeather('-97.7','30.3')
-// -97.743
-// 30.267
-// getInfo(-97.743,30.267)
 // fetch('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civil&output=json')
 // .then(resp => resp.json())
 // .then(data => console.log(data))
